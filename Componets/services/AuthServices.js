8a018bf88b042d11be8ba3b9e055357389cde974
Ignacio Agustin Rojos <99892732+IgnacioRojos@users.noreply.github.com>
2026-09-14 17:52:55 -0300

    Refactor AuthApi for user authentication methods

--- Componets/services/AuthServices.js
+++ Componets/services/AuthServices.js
@@ -1,30 +1,25 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"; 
-import { base_auth_url } from "../firebase/DataBase";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { base_auth_url, API_KEY } from "../firebase/DataBase";
 
+export const AuthApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: base_auth_url }),
+  endpoints: (builder) => ({
+    signUp: builder.mutation({
+      query: (userData) => ({
+        url: `accounts:signUp?key=${API_KEY}`,
+        method: "POST",
+        body: { ...userData, returnSecureToken: true },
+      }),
+    }),
+    logIn: builder.mutation({
+      query: (userData) => ({
+        url: `accounts:signInWithPassword?key=${API_KEY}`,
+        method: "POST",
+        body: { ...userData, returnSecureToken: true },
+      }),
+    }),
+  }),
+});
 
-export const AuthApi = createApi ({
-	reducerPath: "shopApi",
-	baseQuery: fetchBaseQuery({baseUrl: base_auth_url}),
-	endpoints: (builder) => ({
-        signUp:builder.mutation({
-            query : ({...userData}) => ({
-                url:`accounts:signInWithCustomToken?key=${API_KEY}`,
-                method:"POST",
-                body: userData
-            })
-
-            
-        }),
-        logIn:builder.mutation({
-            query : ({...userData}) => ({
-                url:`accounts:signInWithPassword?key=${API_KEY}`,
-                method:"POST",
-                body: userData
-            })
-        })
-    })
-
-})
-
-
-export const {userSignUpMutation, uselogInMutation} = AuthApi
+export const { useSignUpMutation, useLogInMutation } = AuthApi;
